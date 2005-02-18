4435ed673e12a366823d68bb01085859765fd5d3
Aaron Denney <wnoise@ofb.net>
2005-02-17 21:45:54 -0800

    Working yes.  Too simple for GetOpt.

---
+++ yes.hs
@@ -0,0 +1,27 @@
+module Main(main) where
+import System(getArgs)
+import List(intersperse)
+
+main = do args <- getArgs
+          case args of ["--help"]    -> help
+                       ["--version"] -> version
+                       []            -> forever $ putStrLn "y"
+                       _             -> forever $ putStrLn $ concat $ intersperse " " args
+
+forever :: IO () -> IO ()
+forever x = x >> forever x
+
+help :: IO ()
+help = 
+       putStrLn "Usage: yes [STRING]..."
+    >> putStrLn "  or:  yes OPTION"
+    >> putStrLn "Repeatedly output a line with all specified STRING(s), or `y'."
+    >> putStrLn ""
+    >> putStrLn "      --help     display this help and exit"
+    >> putStrLn "      --version  output version information and exit"
+
+version :: IO ()
+version =
+       putStrLn "yes (Haskell educational reimplementation)"
+    >> putStrLn "Written by Aaron Denney"
+    >> putStrLn "Copyright (C) 2005 Aaron Denney"

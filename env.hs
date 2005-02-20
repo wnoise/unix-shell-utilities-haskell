6e63495dae27c22742dcdc19a2904a87480376f2
Aaron Denney <wnoise@ofb.net>
2005-02-20 09:25:50 -0800

    env sets env

--- env.hs
+++ env.hs
@@ -4,8 +4,30 @@ import System.Posix.Env
 
 main = do
        args <- getArgs
-       case args of [] -> dump_environment
-                    _  -> error "unimplemented"
+       handle args
 
 dump_environment :: IO ()
 dump_environment = getEnvironmentPrim >>= (mapM_  putStrLn)
+
+handle [] = dump_environment
+handle (x:xs) | is_env_setting x = set_env x >> handle xs
+handle (x:xs) | is_argument x = handle_arg x >> handle xs
+handle xs = execute xs
+
+is_env_setting = any (== '=')
+
+count pred xs = length $ filter pred xs
+
+set_env = putEnv
+
+is_argument "-i" = True
+is_argument "--ignore-environment" = True
+is_argument "-u" = True
+is_argument "--unset=NAME" = True
+is_argument "--help" = True
+is_argument "--version" = True
+is_argument "-" = True
+
+handle_arg = error "handle_arg is stub."
+
+execute xs = error "execute is stub."
